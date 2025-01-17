9b76c3232b338c626aaec3850f8973ab1bc7ba67
Mechatolex <tob.fiedler@t-online.de>
2025-01-17 22:27:04 +0100

    Add files via upload

--- map.js
+++ map.js
@@ -1,9 +1,24 @@
 // load map
-var map = L.map('map').setView([51.93892, 11.42784], 15);
+var map = L.map('map').fitWorld();
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 20,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 16,
+    attribution: '© OpenStreetMap'
 }).addTo(map);
 
+map.locate({setView: true, maxZoom: 16});
 
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("Deine Position").openPopup();
+}
+
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationerror', onLocationError);
